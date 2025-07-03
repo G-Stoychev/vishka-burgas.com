c877e2ae7f680392a859bfff750d3fe5acd33453
Freak-JStoychev <gstoychev20@gmail.com>
2025-07-03 18:08:42 +0300

    add mroe images

--- imgData.js
+++ imgData.js
@@ -124,6 +124,251 @@ const vishkaImg = [
         name: "Снимка 25",
         alt: "услуга с авто вишка",
     },
+    {
+        img: "images/Vishki/IMG-31220000941f22da54458bea7a92f893-V.jpg",
+        name: "Снимка 26",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-3173aa3c3e3526f8f18ad4264a63e296-V.jpg",
+        name: "Снимка 27",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-31fe01b20fcb461ac42d89dd8b3f0c2d-V.jpg",
+        name: "Снимка 28",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-38a79b7517e5cc0cbe77a0583f6cc683-V.jpg",
+        name: "Снимка 29",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-42c212da79bec9a6390b636df613b794-V.jpg",
+        name: "Снимка 30",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-440ec31ca75f6bdbbd207cc18b53a78f-V.jpg",
+        name: "Снимка 31",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-4478d991db59c4f73865ec801468adac-V.jpg",
+        name: "Снимка 32",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-4914a3a6edfc703377ac245976997a49-V.jpg",
+        name: "Снимка 33",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-5c9ba2eb99647ffd35c4b3fd22f475ef-V.jpg",
+        name: "Снимка 34",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-5d075e69cb5f974f9cf242898432220b-V.jpg",
+        name: "Снимка 35",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-6363282efab93d50f28c0c07f5a8d08a-V.jpg",
+        name: "Снимка 36",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-646cf77591c435afa1b099bbdfa03307-V.jpg",
+        name: "Снимка 37",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-678ef2c68256c7f21a1611034e35407a-V.jpg",
+        name: "Снимка 38",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-6a16d6f7979a59afe8ff69355528ca25-V.jpg",
+        name: "Снимка 39",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-79e7d92abcc6ce7b79973451bb32ab92-V.jpg",
+        name: "Снимка 41",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-79f0c50580a4c090c0f1c0696c2b5741-V.jpg",
+        name: "Снимка 42",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-7c2a1d2b19a32ce26ec4d7e9769a7fa2-V.jpg",
+        name: "Снимка 43",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-80c53fc081aa0e511926dc106d56ed85-V.jpg",
+        name: "Снимка 44",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-80cd3f11562651c5e83e17328f70b297-V.jpg",
+        name: "Снимка 45",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-829616a8305e409be1b0c9120cb3897f-V.jpg",
+        name: "Снимка 46",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-82ce9a7f8d61b10e8b54069e16fb483d-V.jpg",
+        name: "Снимка 47",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-831442fa6d466a80553a387d9a6133d8-V.jpg",
+        name: "Снимка 48",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-8333b47546a48b135100d44ba9b868a4-V.jpg",
+        name: "Снимка 49",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-8496516878603ccadf6ac9ad3e625998-V.jpg",
+        name: "Снимка 50",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-8707f671d1a6445d7f80f542ea49a978-V.jpg",
+        name: "Снимка 51",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-8df47532d05319bf481fc2ed5b9c90e1-V.jpg",
+        name: "Снимка 52",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-8f358eb3b892d311e8d6b335463f00d6-V.jpg",
+        name: "Снимка 53",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-928fd874b91fa92862219f4d8fcf32d8-V.jpg",
+        name: "Снимка 54",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-93582ae9290fb3cf5a3cba71bd32a778-V.jpg",
+        name: "Снимка 55",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-95786efed862f6e2e91db8064f855d39-V.jpg",
+        name: "Снимка 56",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-974067d5993ed3de26449608323e08c9-V.jpg",
+        name: "Снимка 57",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-9a54a94c189049d8b99716c428c9759e-V.jpg",
+        name: "Снимка 58",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-a606c524f6a502e6879e39b126f9b9e7-V.jpg",
+        name: "Снимка 59",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-ab9e24012c037f8b1d14306f6a41672b-V.jpg",
+        name: "Снимка 60",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-ad869ce3836840acb01202d0c5e7286e-V.jpg",
+        name: "Снимка 61",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-af0649099b682e5c2d0becedfc42a4a2-V.jpg",
+        name: "Снимка 62",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-bb246df6233ad9c99f483202c75f7f26-V.jpg",
+        name: "Снимка 63",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-be58070ec13d9b625995134e83b0d8ab-V.jpg",
+        name: "Снимка 64",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-c1ef96444e2e94e1b20268ffbc607498-V.jpg",
+        name: "Снимка 65",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-c4c2186d01aa3bc94a89c7c02bb8aad8-V.jpg",
+        name: "Снимка 66",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-ceef6513880e1e134076b2b61e862848-V.jpg",
+        name: "Снимка 67",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-d1757c04fafa9fda36b4a91656034242-V.jpg",
+        name: "Снимка 68",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-de7fc2e6cbde75143238aa6d5fec38e0-V.jpg",
+        name: "Снимка 69",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-df14eccbeba5410e9527f608fa4cceba-V.jpg",
+        name: "Снимка 70",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-f02bad95b3d7cbcf662a701ad645847c-V.jpg",
+        name: "Снимка 71",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-f2255c3e60ee21b72fc6b78effb4b0d6-V.jpg",
+        name: "Снимка 72",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-f3c5bdd85957992c97c2af608ff01ee8-V.jpg",
+        name: "Снимка 73",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-f5aee26173dda96dd04f0b68c01ed559-V.jpg",
+        name: "Снимка 74",
+        alt: "услуга с авто вишка",
+    },
+    {
+        img: "images/Vishki/IMG-f9e955fce135f17d1c0023a554313b8a-V.jpg",
+        name: "Снимка 75",
+        alt: "услуга с авто вишка",
+    },
 ];
 
 export default vishkaImg;
